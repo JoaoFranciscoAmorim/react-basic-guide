f3984c0fcce8890549d29619fc96f15f452e325f
joaoamorim <joao.amorim.ga@gmail.com>
2021-05-04 14:22:59 -0300

    event

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,27 +1,32 @@
 import "./NewExpense.css";
 
 function NewExpense() {
+  function titleChangeHandler(event) {
+    console.log(event.target.value)
+  };
   return (
     <div className="new-expense">
       <form>
-          <div className='tit'>
+        <div className='new-expense__controls'>
+          <div className='new-expense__control'>
             <label>Title</label>
-            <input type="text"></input>
+            <input type="text" onChange={titleChangeHandler}></input>
           </div>
-          <div className='tit'>
+          <div className='new-expense__control'>
             <label>Amount</label>
             <input type="number" min="0.01" step="0.01"></input>
           </div>
-          <div className='tit'>
+          <div className='new-expense__control'>
             <label>Date</label>
             <input type="date" min="2019-01-01" max="2022-12-31"></input>
           </div>
-          <div>
+          <div className='new-expense__actions'>
               <button type="submit">Add Expense</button>
+          </div>
           </div>
       </form>
     </div>
   );
 }
-
+/*-O evento onChange*/
 export default NewExpense;
